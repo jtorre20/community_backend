7eda05cde9d59f6782d0ca6c0e54933e552d0085
jtorre20 <jtorres1989@gmail.com>
2018-07-25 19:09:50 -0400

    updated users and moods controller

--- community/app/controllers/moods_controller.rb
+++ community/app/controllers/moods_controller.rb
@@ -14,9 +14,10 @@
     def create
       url = "https://api.aylien.com/api/v1/sentiment"
       new_mood = Mood.create(mood_params)
-      # byebug
       result = RestClient.post(url,{mode:params[:mode], language:params[:language], text:params[:text]}, {"X-AYLIEN-TextAPI-Application-Key": "73ead8b493c2bff21192d56d9709a188", "X-AYLIEN-TextAPI-Application-ID": "3d781275", "Content-Type": "application/json",Accept: "application/json"})
-      render json: {mood: result}
+      # byebug
+      parsed_result = JSON.parse(result)
+      render json: {mood: parsed_result}
     end
   
     def destroy
@@ -32,7 +33,6 @@
     private
   
     def mood_params
-      # user_id = Auth.decode(params[:user_id])["user_id"]
         {:user_id => Auth.decode(params[:user_id])["user_id"], 
           :text => params[:text], 
           :mood => params[:mood], 
--- community/db/schema.rb
+++ community/db/schema.rb
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2018_07_17_193359) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
